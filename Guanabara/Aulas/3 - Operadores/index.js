2694bad99c76c82c63e6a416cbb986b0bfe4de4a
Vinicius Baessi <viniciusbaessi@gmail.com>
2023-02-03 14:38:21 -0300

    save

---
+++ Guanabara/Aulas/3 - Operadores/index.js
@@ -0,0 +1,82 @@
+/*
+
+Operadores Aritméticos:
+ +     // Somar   
+  -    // Subtrair
+  *    // Multiplicação 
+  /    // divisão
+ %   // Resto de uma divisão 
+ **   // Potencia do primeiro numero elevado ao segundo
+
+
+
+5 + 3   = 8
+5 - 3    = 2
+5 * 3    = 15
+5 / 3    = 1,6
+5 % 3  =  2
+5 ** 3   = 125
+
+Ordem de precedencia dos operadores aritiméticos ↓
+
+ ( ) 
+ **
+ /   *   %
+ +  -
+
+
+Existem duas formas de escrever o comando abaixo.
+
+var = var + 2
+var +=2
+
+ou
+
+var = var - 2
+var -=2
+
+
+
+Operadores relacionais
+
+> Maior
+< Menor
+>= Maior ou igual
+<= Menor ou igual
+== Igual
+!= Diferente de
+
+
+Operadores de identidade
+
+5 == 5 True
+5 == '5' true (aqui ele verifica se os valores são iguais e não o seu tipo)
+5 === '5' False (aqui ele verifica se os valores e o tipo são iguais, no caso temos um número e uma string)
+
+Operadores lógicos
+
+!         NEGAÇÃO
+&&        CONJUNÇÃO (E)
+||        DISJUNÇÃO (OU)
+
+
+Ordem de precedencia dos operadores em geral↓
+
+aritiméticos
+relacionais
+lógicos
+
+
+Operador ternário
+
+TESTE ? true : false
+
+
+Exemplo:
+var média = 5.5
+undefined
+média >= 7.0 ? "APROVADO" : "REPROVADO"
+'REPROVADO'
+
+
+*/
