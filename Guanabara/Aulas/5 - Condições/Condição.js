97614337853f9e23d3bf04ab3529d91392f675cb
Vinicius Baessi <viniciusbaessi@gmail.com>
2023-02-06 13:32:23 -0300

    save

--- Guanabara/Aulas/5 - Condições/Condição.js
+++ Guanabara/Aulas/5 - Condições/Condição.js
@@ -30,3 +30,97 @@ if (país == 'Brasil') {
 else {
     console.log('Você é estrangeiro')
 }
+
+
+
+
+
+
+
+// condições aninhadas (IF > ELSE IF)
+
+var idade = 22
+
+if (idade < 16) {
+    console.log ('Não vota')
+
+}   else if ( idade >= 16 && idade <18) {
+        console.log('Voto opcional')
+} 
+    else if (idade >= 18) {
+        console.log('Voto obrigatório')
+}
+
+
+
+
+var agora = new Date()
+var hora = agora.getHours()
+
+console.log(`Agora é exatamente ${hora} horas.`)
+if (hora < 12) {
+    console.log('Bomd dia')
+} else if (hora <= 18) {
+    console.log('Boa tarde')
+} else {
+    console.log('Boa noite')
+}
+
+
+
+
+// condições múltiplas (switch)
+
+var agora = new Date()
+var diaSem = agora.getDay()
+
+/* 
+
+0 - DOM
+1 - SEG
+2 - TER
+3 - QUA
+4 - QUI
+5 - SEX
+6 - SAB
+
+*/
+
+switch(diaSem) {
+    case 0:
+        console.log('Domingo')
+        break
+        
+
+        case 0:
+            console.log('Domingo')
+            break
+
+        case 1:
+            console.log('Segunda')
+            break
+
+        case 2:
+            console.log('Terça')
+            break
+
+        case 3:
+            console.log('Quarta')
+            break
+
+        case 4:
+            console.log('Quinta')
+            break
+
+        case 5:
+            console.log('Sexta')
+            break
+
+        case 6:
+            console.log('Sábado')
+            break
+
+        default:
+            console.log('[ERRO] Dia inválido')
+            break
+}
