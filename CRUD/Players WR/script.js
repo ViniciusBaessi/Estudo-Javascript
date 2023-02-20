4f365f7b1c457d9706492b35eb6eb0dfc9c4be44
Vinicius Baessi <viniciusbaessi@gmail.com>
2023-02-20 18:58:12 -0300

    save

--- CRUD/Players WR/script.js
+++ CRUD/Players WR/script.js
@@ -1,5 +1,5 @@
-console.log('Olá')
 
+//CREATE --------------------------------------------------------------------
 
 // Linkando a tag (form) na variável $meuForm
 const $meuForm = document.querySelector('form')
@@ -11,5 +11,19 @@ $meuForm.addEventListener('submit', function EnviaDadoController(InfosDoEvento) 
 
     // Faz com que a página não recarregue mantendo os eventos nela
     InfosDoEvento.preventDefault();
-    console.log('Lararu')
+
+    // Linkando a tag (input) na variável $EnviaPost
+    const $EnviaPost = document.querySelector('input[name="EnviaPost"]')
+
+    // Linkando a tag (ul) na variável $ListaEnvio
+    const $ListaEnvio = document.querySelector('.ListaEnvio')
+
+    //Pritando a resposta após a tag ul, inserindo um (li)
+    $ListaEnvio.insertAdjacentHTML('afterbegin',`<li>${$EnviaPost.value}</li>` )
+
+    //Receber o valor, a variável fica fazia
+    $EnviaPost.value = ''
+
+    
+
 })
