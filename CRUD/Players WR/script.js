9b761631094ebedf45412f4185b399ccb1950d12
Vinicius Baessi <viniciusbaessi@gmail.com>
2023-02-20 17:05:57 -0300

    save

--- CRUD/Players WR/script.js
+++ CRUD/Players WR/script.js
@@ -1,1 +1,15 @@
 console.log('Olá')
+
+
+// Linkando a tag (form) na variável $meuForm
+const $meuForm = document.querySelector('form')
+console.log($meuForm)
+
+
+//Toda vez que o botão enviar for clicado, faça a função ...
+$meuForm.addEventListener('submit', function EnviaDadoController(InfosDoEvento) {
+
+    // Faz com que a página não recarregue mantendo os eventos nela
+    InfosDoEvento.preventDefault();
+    console.log('Lararu')
+})
