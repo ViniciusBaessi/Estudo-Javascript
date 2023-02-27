d75f17e0c06e4c81daa0189481370d012c451e35
Vinicius Baessi <viniciusbaessi@gmail.com>
2023-02-27 14:25:13 -0300

    save

--- CRUD/Meu projeto/script.js
+++ CRUD/Meu projeto/script.js
@@ -11,6 +11,8 @@ function limpa_dados () {
 //Linkando o form de nome em uma variável
 var nome = window.document.getElementById('nome')
 var idade = window.document.getElementById('idade')
+var curso = window.document.getElementById('curso')
+
 var res = window.document.getElementById('res')
 var msg = window.document.getElementById('msg')
 var form = window.document.getElementById('salvar_alterar')
@@ -31,7 +33,7 @@ function adicionar_usuario () {
 
 
     //Criando um objeto chamado (usuário) e preenchendo com os dados inputados pelo usuário
-    const usuario = { nome: `${nome.value}`, idade: `${idade.value}` };
+    const usuario = { nome: `${nome.value}`, idade: `${idade.value}`, curso: `${curso.value}` };
 
    
     
@@ -48,6 +50,7 @@ function adicionar_usuario () {
     //Esvaziando o formulário para inserção de novos dados
     nome.value = ""
     idade.value = ""
+    curso.value = ""
 
 
     //Acionando a função ler dados
@@ -77,9 +80,9 @@ function ler_usuario () {
 
         //Inserindo os dados na tela junto ao botão para UPDATE e DELETE
         res.innerHTML += 
-        `Nome: ${db_usuario[contador].nome} | idade: ${db_usuario[contador].idade} 
+        `Nome: ${db_usuario[contador].nome} | idade: ${db_usuario[contador].idade} | curso: ${db_usuario[contador].curso} </div>
         
-        <button id="editar" data-indice="${contador}" onclick="altera_usuario()">Editar</button> <button id="excluir" onclick="exclui_usuario()">Excluir</button> <br>`
+        <button id="editar" data-indice="${contador}" onclick="altera_usuario()">Editar</button> <button id="excluir" data-indice="${contador}" onclick="exclui_usuario()">Excluir</button> <br>`
         
 
         contador = contador + 1
@@ -117,6 +120,7 @@ function altera_usuario() {
     //Filtra os dados através da variável (indice) e insere os valores correspondentes no formulário
     nome.value = db_usuario[indice].nome
     idade.value = db_usuario[indice].idade
+    curso.value = db_usuario[indice].curso
 
 
 
@@ -143,6 +147,7 @@ function salvar_usuario (aux) {
     //O objeto salvo na (db_usuario) recebe os dados inputados no formulário
     db_usuario[aux].nome = nome.value
     db_usuario[aux].idade = idade.value
+    db_usuario[aux].curso = curso.value
     
     // Os dados na variável db_usuario são convertidos de JSON para string e é armazenado no localstorage
     localStorage.setItem("db_usuario", JSON.stringify(db_usuario));
@@ -191,26 +196,38 @@ function botao_alterar () {
  //-------------------------- DELETE --------------------------------------------
 
 
+ function exclui_usuario () {
+  
+    //Retorna o valor do indice do botão que foi clicado e é armazenado na variável (indice)
+    const indice = event.target.dataset.indice;
+
+    //Pega os dados do (localstorage) e convertendo para JSON
+    const db_usuario = JSON.parse(localStorage.getItem('db_usuario')) ?? []
+
+    //Remove um elemento (nome e idade) de acordo com o indice na variável (db_usuario)
+    db_usuario.splice(indice, 1);
+
+    // Os dados na variável db_usuario são convertidos de JSON para string e é armazenado no localstorage
+    localStorage.setItem("db_usuario", JSON.stringify(db_usuario));
+
+    //É feito um reload na página para que o usuario possa realizar novas operações
+    location.reload()
+
+    
+}
 
 
 
 
+ function click (evento) {
+    
+    //Reconhecendo o click no botão editar através do id 
+    if (evento.target.id == 'excluir'){
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        //Printa no console o valor do botão que foi clicado
+        console.log(evento.target.dataset.indice);
+        
+    }
+    
+}
